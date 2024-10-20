f6637a37c524fedde88b199675f38f680be76b81
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2024-10-19 22:36:30 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Audiowide, Poppins } from 'next/font/google';
 import "./globals.css";
 import { Footer } from "@/components/Sections/Footer";
 import { Header } from "@/components/Sections/Header";
+import { AdSense } from '@/Scripts/ScriptAds';
 
 const poppins = Poppins({
   weight: ['400', '700'], 
@@ -27,6 +28,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br" className={`${poppins.variable} ${audiowide.variable} antialiased bg-gradient-to-r from-[#000000] via-[#3A00CC] to-[#000000]`}>
+       <head>
+        <AdSense pId='' />
+      </head>
       <body className={`${poppins.variable}`}>
         <Header />
         {children}
@@ -35,3 +39,66 @@ export default function RootLayout({
     </html>
   );
 }
+
+
+
+//Script ADS 
+import Script from "next/script";
+import React from "react";
+
+type AdSenseTypes = {
+    pId: string;
+}
+
+export const AdSense = ({pId}: AdSenseTypes) => {
+    return (
+        <Script async
+        src={``}
+        crossOrigin="anonymous"
+        strategy="afterInteractive" 
+        />
+    )
+}
+
+
+
+//AdBanner
+'use client'
+
+import React, { useEffect } from "react"
+
+type AdBannerTypes = {
+    dataAdSlot: string;
+    dataAdFormat: string;
+    dataFullWidthResponsive: boolean;
+}
+
+export const AdBanner = ({ dataAdFormat, dataAdSlot, dataFullWidthResponsive}: AdBannerTypes) => {
+
+    useEffect(() => {
+        try {
+            ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push({})
+        }
+        catch(error: any) {
+            console.log(error.message)
+        }
+    }, [])
+
+    return (
+        <ins
+        className="adsbygoogle"
+        style={{display: 'block'}}
+        data-ad-client="ca-pub-123456789"
+        data-ad-slot={dataAdSlot}
+        data-ad-format={dataAdFormat}
+        data-full-width-responsive={dataFullWidthResponsive.toString()}
+        >
+
+        </ins>
+    )
+}
+
+//Banner of page blogs
+ <div className="mb-5">
+                <AdBanner dataAdFormat="auto" dataFullWidthResponsive={true} dataAdSlot="090909090" />
+            </div>
